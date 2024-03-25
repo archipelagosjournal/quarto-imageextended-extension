3c2a4b94baa2afc92f9035e11aeb441cc7c4fabb
Brian Ballsun-Stanton <denubisx@noreply.github.com>
2024-03-25 18:28:12 +1100

    image-ext works
    Signed-off-by: Brian Ballsun-Stanton <denubisx@noreply.github.com>

--- _extensions/image-ext/image-ext.lua
+++ _extensions/image-ext/image-ext.lua
@@ -34,24 +34,48 @@ return {
         -- <figcaption>Suomi satellite images of Puerto Rico on 24 July 2017 and 24 September 2017. Image credit: NOAA National Environmental Satellite, Data, and Information Service (NESDIS).</figcaption>
         -- </figure>
         -- </div>
-
+        local elements = {
+          pandoc.RawInline('html', '<div class="quarto-figure quarto-figure-center">\n<figure class="figure">\n<p>')          
+        }
         if url then
-          html = '<div class="quarto-figure quarto-figure-center">\n<figure class="figure">\n<p><a href="' .. url .. '"><img src="' .. img .. '" class="img-fluid figure-img" alt="' .. caption .. '"></p>\n<figcaption>' .. caption .. '</figcaption></a>\n</figure>\n</div>'
-        else
-          html = '<div class="quarto-figure quarto-figure-center">\n<figure class="figure">\n<p><img src="' .. img .. '" class="img-fluid figure-img" alt="' .. caption .. '"></p>\n<figcaption>' .. caption .. '</figcaption>\n</figure>\n</div>'
+          table.insert(elements, pandoc.RawInline('html', '<a href="' .. url .. '">'))
         end
-        return pandoc.RawInline('html', html)
+        table.insert(elements, pandoc.RawInline('html', '<img src="' .. img .. '" class="img-fluid figure-img" alt="'))
+        table.insert(elements, pandoc.Str(caption))
+        table.insert(elements, pandoc.RawInline('html', '"></p>\n<figcaption>'))
+        table.insert(elements, pandoc.Str(caption))
+        table.insert(elements, pandoc.RawInline('html', '</figcaption>'))
+        if url then
+          table.insert(elements, pandoc.RawInline('html', '</a>'))
+        end
+        table.insert(elements, pandoc.RawInline('html','\n</figure>\n</div>'))
+
+        return pandoc.Para(elements)
     elseif quarto.doc.is_format("latex") then
       -- if there's a url, we need to append it to the print caption for those who are working on a printed out version of the pdf. We also need to make the image clickable.
+      
       if url then
         print_caption = print_caption .. " \\href{" .. url .. "}{(" .. url .. ")}"
-        latex = '\\begin{figure}[H]\n\\centering\n\\href{' .. url .. '}{\\includegraphics[width=\\textwidth]{' .. img .. '}}\n\\caption{' .. print_caption .. '}\n\\end{figure}'
-      else
-        latex = '\\begin{figure}[H]\n\\centering\n\\includegraphics[width=\\textwidth]{' .. img .. '}\n\\caption{' .. print_caption .. '}\n\\end{figure}'
       end
-      quarto.log.output("latex = ", latex)
 
-      return pandoc.RawInline('latex', latex)
+      local latex = {
+        pandoc.RawInline('latex', '\\begin{figure}[H]\n\\centering\n')
+      }
+      if url then
+        table.insert(latex,pandoc.RawInline('latex','\\href{'..url..'}{'))
+      end
+      table.insert(latex,pandoc.RawInline('latex','\\includegraphics[width=\\textwidth]{' .. img .. '}\n'))
+      if url then
+        table.insert(latex,pandoc.RawInline('latex','}'))
+      end
+      table.insert(latex,pandoc.RawInline('latex','\n\\caption{' .. print_caption .. '}\n\\end{figure}'))
+      
+      -- else
+      --   latex = '\\begin{figure}[H]\n\\centering\n\\includegraphics[width=\\textwidth]{' .. img .. '}\n\\caption{' .. print_caption .. '}\n\\end{figure}'
+      -- end
+      -- quarto.log.output("latex = ", latex)
+
+      return pandoc.Para(latex)
     
     end
     return pandoc.Str("[" .. img .. (caption ~= "" and " -- " .. caption or "") .. "]")
